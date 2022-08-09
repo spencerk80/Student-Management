72a7f34b8710dd8b528f7e5f1a37811b692617aa
Kristoffer Spencer <whobanil80@linuxmail.org>
2022-08-09 06:40:27 -0700

    Created models

---
+++ src/main/java/com/github/spencerk/student_management/model/Course.java
@@ -0,0 +1,62 @@
+package com.github.spencerk.student_management.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int     id;
+    private String  name,
+                    instructor;
+
+    public Course(String name, String instructor) {
+        this.name = name;
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Course course;
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        course = (Course) o;
+
+        return id == course.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Course{id=%d,\n\tname=%s,\n\tinstructor=%s}", id, name, instructor);
+    }
+}
---
+++ src/main/java/com/github/spencerk/student_management/model/Student.java
@@ -0,0 +1,101 @@
+package com.github.spencerk.student_management.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int             id;
+    private String          fname,
+                            lname,
+                            email,
+                            phone;
+    private List<Course>    courses;
+
+    public Student(String fname, String lname, String email, String phone, List<Course> courses) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phone = phone;
+        this.courses = courses;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Student student;
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        student = (Student) o;
+
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder courseSB = new StringBuilder("Courses[");
+
+        courses.forEach(course -> courseSB.append(String.format("\n\t\n\t%s", course)));
+        courseSB.append("\n\t]");
+
+        return String.format(
+                "Student{\n\tid=%d,\n\tfirstname=%s,\n\tlastname=%s,\n\temail=%s,\n\tphone=%s,\n\tcourses=%s}",
+                id, fname, lname, email, phone, courseSB.toString()
+        );
+    }
+}
